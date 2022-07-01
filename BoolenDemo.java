568fdc7d854a3d5ecc924aaf0b07784e8e6719f5
Duke <hibary.taiwan@hotmail.com>
2022-07-01 21:35:35 +0800

    feat: Boolen demo

---
+++ BoolenDemo.java
@@ -0,0 +1,11 @@
+public class BoolenDemo {
+    // 1.console function
+    public static void main(String[] args) {
+        // 2.boolen variable declaration
+        Boolean isHot = true;
+        if(isHot){
+            System.out.println("Eat icecream!");
+        }
+
+    }
+}
